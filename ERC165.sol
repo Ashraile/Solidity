35d11fb161653436a8d165b840b9c0d0a8f176f3
Ashraile <89170464+Ashraile@users.noreply.github.com>
2024-01-21 15:09:08 -0600

    Update ERC165.sol (version 3)

--- ERC165.sol
+++ ERC165.sol
@@ -1,13 +1,13 @@
 /** SPDX-License-Identifier: MIT 
-    @title ERC165 To-spec Implementation
-    @custom:improv Ashraile
+    @title ERC165.sol (To-spec implementation)
+    @custom:improv Ashraile 
+    @custom:version 3.0
 */
 pragma solidity ^0.8.21;
 
-/// @notice IID: 0x01ffc9a7
+/// @dev Returns true if the contract supports the given interface. @notice IID: 0x01ffc9a7
 interface IERC165 {
-    error ERC165InvalidInterface();
-    function supportsInterface(bytes4 IID) external view returns (bool); /// @dev Returns true if the contract supports the given interface.
+    function supportsInterface(bytes4 IID) external view returns (bool);
 }
 
 /**
@@ -15,18 +15,15 @@ interface IERC165 {
  * Derived contracts need only register support for their own interfaces; we register support for ERC165 itself here.
  */
 abstract contract ERC165 is IERC165 {
-
-    constructor() { 
-        _registerInterface(0x01ffc9a7); // bytes4(keccak256('supportsInterface(bytes4)')) == 0x01ffc9a7
-    }
+    constructor() { _registerInterface(0x01ffc9a7); } // bytes4(keccak256('supportsInterface(bytes4)')) == 0x01ffc9a7
 
     /// @dev See {IERC165-supportsInterface}. Time complexity O(1), guaranteed to always use less than 30 000 gas.
-    mapping(bytes4 IID => bool) public supportsInterface;
+    mapping (bytes4 IID => bool) public supportsInterface;
     
-    /// @dev Registers the contract as an implementer of an interface. `interfaceId` cannot be the ERC165 invalid interface (`0xffffffff`).
-    function _registerInterface(bytes4 IID) internal virtual {
-        if (IID == 0xffffffff) { revert ERC165InvalidInterface(); }
-        supportsInterface[IID] = true;
+    /// @dev Registers the contract as an implementer of an interface. `interfaceId` cannot be the ERC165 invalid interface `0xffffffff`.
+    function _registerInterface(bytes4 IID) internal virtual { 
+        supportsInterface[IID] = (IID != 0xffffffff);
     }
 }
 
+contract TestInterface is ERC165 {}
