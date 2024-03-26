edf8d05154795903efd63a0381de15adb5d1893c
Ashraile <89170464+Ashraile@users.noreply.github.com>
2024-03-26 17:38:21 -0500

    Update Reflectable.sol

--- Reflectable.sol
+++ Reflectable.sol
@@ -50,73 +50,6 @@ interface IERC20MetadataExtended is IERC20Metadata {
 }
 
 
-interface IERC721 is IERC165 {
-
-
-}
-
-/** @custom:credit Remco Bloemen (https://xn--2-umb.com/21/muldiv)
-    @custom:improv Uniswap Labs 
-    @custom:source OpenZeppelin (Math library)
-    @custom:license MIT
-*/
-library Math {
-
-    /** @dev Calculates floor(x * y / denominator) with full precision. Throws if result overflows a uint256 or denominator == 0. 
-        Handles "phantom overflow" i.e., allows multiplication and division where an intermediate value overflows 256 bits. */
-    error MathOverflowedMulDiv();
-
-    function mulDiv(uint x, uint y, uint d) internal pure returns (uint) {  unchecked  {
-
-        // 512-bit multiply [prod1 prod0] = x*y. Compute the product mod 2^256 and mod 2^256 - 1, then use the Chinese Remainder Theorem to reconstruct the 512 bit result. 
-        // The result is stored in two 256 variables such that product = prod1 * 2^256 + prod0. 
-
-        uint prod0 = x * y; uint prod1; // (Least, Most) significant 256 bits of the product.
-        assembly {
-            let mm := mulmod(x, y, not(0))
-            prod1 := sub(sub(mm, prod0), lt(mm, prod0))
-        }
-
-        // 256 by 256 division
-        if (prod1 == 0) { return prod0 / d; }
-        // Solidity will revert if denominator == 0, unlike the div opcode on its own.
-        // The surrounding unchecked block does not change this fact.
-        /// @custom:ref https://docs.soliditylang.org/en/latest/control-structures.html#checked-or-unchecked-arithmetic.
-
-        if (d <= prod1) { revert MathOverflowedMulDiv(); } // Make sure the result is less than 2^256. Also prevents denominator == 0.
-
-        // 512 by 256 division
-        uint remainder; // Make division exact by subtracting the remainder from [prod1 prod0].
-        assembly {
-            remainder := mulmod(x, y, d)    // Compute remainder using mulmod.
-            prod1 := sub(prod1, gt(remainder, prod0)) // Subtract 256 bit number from 512 bit number.
-            prod0 := sub(prod0, remainder)
-        }
-
-        uint twos = d & (0 - d);                      // Factor powers of two out of denominator and compute largest power of two divisor of denominator. Always >= 1.
-        assembly {                                    /// @custom:ref https://cs.stackexchange.com/q/138556/92363.
-            d := div(d, twos)                         // Divide denominator by twos.
-            prod0 := div(prod0, twos)                 // Divide [prod1 prod0] by twos.
-            twos := add(div(sub(0, twos), twos), 1)   // Flip twos such that it is 2^256 / twos. If twos is zero, then it becomes one.
-        }
-
-        prod0 |= prod1 * twos; // Shift in bits from prod1 into prod0.
-
-        // Invert denominator mod 2^256. Now that denominator is an odd number, it has an inverse modulo 2^256 such that denominator * inv = 1 mod 2^256.
-        // Compute the inverse by starting with a seed that is correct for four bits. That is, denominator * inv = 1 mod 2^4.
-        uint inv = (3 * d) ^ 2;
-
-        inv *= 2 - d * inv; // inverse mod 2**8     ~   Use the Newton-Raphson iteration to improve the precision.
-        inv *= 2 - d * inv; // inverse mod 2**16    ~   Thanks to Hensel's lifting lemma, this also works in modular arithmetic, doubling the correct bits in each step.
-        inv *= 2 - d * inv; // inverse mod 2**32    ~   
-        inv *= 2 - d * inv; // inverse mod 2**64    ~   Because the division is now exact we can divide by multiplying with the modular inverse of the denominator.
-        inv *= 2 - d * inv; // inverse mod 2**128   ~   This will give us the correct result modulo 2^256. Since the preconditions guarantee that the outcome is less than 2^256,
-        inv *= 2 - d * inv; // inverse mod 2**256   ~   this is the final result. We don't need to compute the high bits of the result and prod1 is no longer required.
- 
-        return prod0 * inv;
-    }}
-}
-
 /**
  * @dev Implementation of the {IERC165} interface. Contracts may inherit from this and call {_registerInterface} to declare their support of an interface.
  * Derived contracts need only register support for their own interfaces; we register support for ERC165 itself here.
@@ -180,14 +113,17 @@ abstract contract Ownable is Context {
     }
 
     function lowerOwnerAccessBy1() onlyOwner external { // Lowers access level for the owner. Once lowered it cannot be increased.
-        if (sudo == 0) { return; } emit AccessChanged(--sudo, block.timestamp);
+        if (sudo == 0) { return; } 
+        emit AccessChanged(--sudo, block.timestamp);
     }
 
     function _revokeOwnerPrivileges() private { 
-        if (sudo == 0) { return; } sudo = 0; emit AccessChanged(0, block.timestamp);
+        if (sudo == 0) { return; } 
+        sudo = 0; 
+        emit AccessChanged(0, block.timestamp);
     }
 
-    function revokeOwnerPrivileges() onlyOwner external { _revokeOwnerPrivileges(); }
+    function revokeOwnerPrivileges() external onlyOwner() { _revokeOwnerPrivileges(); }
      
     /** @dev Transfers ownership of the contract to a new account (`newOwner`). Can only be called by the current owner.
      * Will also leave any tokens owned, in possession of the previous owner.
@@ -219,7 +155,7 @@ interface IERC20CustomErrors {
 
 
 /** @author Jasper Wolf (https://www.github.com/Ashraile)  Requires Solidity 0.8+
-    @dev Advanced customizable ERC20 (BEP20) token generation framework. @custom:ref https://github.com/bnb-chain/BEPs/blob/master/BEPs/BEP20.md
+    @dev Next-gen ERC20 (BEP20) token generation framework. @custom:ref https://github.com/bnb-chain/BEPs/blob/master/BEPs/BEP20.md
 */
 abstract contract ERC20 is Context, IERC20, IERC20Metadata, IERC20CustomErrors, Ownable {
 
@@ -496,4 +432,3 @@ function TestToken is ERC20Reflectable, ERC165 {
     }
     receive() external payable {}
 }
-    
