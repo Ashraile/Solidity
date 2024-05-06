8f357ea791405dbfd928ee6a27a10727080dc6c6
Ashraile <89170464+Ashraile@users.noreply.github.com>
2024-05-06 01:35:52 -0500

    Create Prototype.sol
    Gas estimate helper function.

---
+++ Prototype.sol
@@ -0,0 +1,18 @@
+/// SPDX-License-Identifier: MIT
+/// @author Ashraile
+
+pragma solidity >=0.8.21 <0.9.0;
+
+import "hardhat/console.sol";
+
+/**
+ * @dev Prototyping QOL. The GAS opcode itself costs 2 gas
+ */
+abstract contract Prototype {
+    modifier gas() {
+        uint _gas = gasleft();
+         _; 
+        console.log(_gas - gasleft());
+    }
+    bool internal immutable REMIX = (block.chainid == 1 && block.number < 1e6);
+}
