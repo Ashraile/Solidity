34bb14195e168333caa2460f7c1fd5f6bbdbb2e0
Ashraile <89170464+Ashraile@users.noreply.github.com>
2024-05-06 01:39:32 -0500

    Update Prototype.sol

--- Prototype.sol
+++ Prototype.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.8.21 <0.9.0;
 import "hardhat/console.sol";
 
 /**
- * @dev Prototyping QOL. The GAS opcode itself costs 2 gas
+ * @dev Prototyping QOL. NOTE: The GAS opcode itself costs 2 gas
  */
 abstract contract Prototype {
     modifier gas() {
